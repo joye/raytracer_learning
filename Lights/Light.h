5d47465447f330157090c34ca9e6ec98ba86cfba
Ye Wei <yewei86@gmail.com>
2016-05-19 14:48:24 +0800

    first submit
    update all frameworks at the beginning

---
+++ Lights/Light.h
@@ -0,0 +1,36 @@
+#ifndef __LIGHT__
+#define __LIGHT__
+
+#include "Utilities/Vector3D.h"
+#include "Utilities/RGBColor.h"
+#include "Utilities/Ray.h"
+
+class ShadeRec;
+
+
+//-------------------------------------------------------------------- class Light
+
+class Light {	
+	public:
+	
+		Light(void);
+								
+		Light(const Light& ls);			
+
+		Light& 								
+		operator= (const Light& rhs); 
+
+		virtual Light* 						
+		clone(void) const = 0;
+		
+		virtual 							
+		~Light(void);
+						
+		virtual Vector3D								
+		get_direction(ShadeRec& sr) = 0;				
+																
+		virtual RGBColor														
+		L(ShadeRec& sr);								
+};
+
+#endif
---
+++ Materials/Material.h
@@ -0,0 +1,30 @@
+#ifndef __MATERIAL__
+#define __MATERIAL__
+
+#include "World/World.h"			// required for the shade function in all derived classes
+#include "Utilities/RGBColor.h"
+#include "Utilities/ShadeRec.h"
+
+class Material {	
+	public:
+	
+		Material(void);						
+		
+		Material(const Material& material); 
+		
+		virtual Material*								
+		clone(void) const = 0;	
+				
+		virtual 								
+		~Material(void);
+				
+		virtual RGBColor
+		shade(ShadeRec& sr);	
+		
+	protected:
+	
+		Material& 								
+		operator= (const Material& rhs);						
+};
+
+#endif
